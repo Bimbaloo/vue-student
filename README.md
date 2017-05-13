217dfd25aad98c83084fb45a13bb114a7cb97817
mazao <1030102244@qq.com>
2017-05-13 15:57:18 +0800

    模块化初探

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ## vue的项目学习
 
 复习了webpage的使用和vue的简单用法
+
+
+cp -r step-4 step-5
+cd step-5
+# 如果是 Windows 系统，再执行下面两行
+rm -rf node_modules
+npm i
+# 结束 Windows 系统的命令
+webpack --watch
+# 然后新开一个窗口
---
+++ resumer/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Topbar/>
+    <ResumeEditor/>
+    <ResumePreview/>
+  </div>
+</template>
+
+<script>
+  import Topbar from './components/Topbar'
+  import ResumeEditor from './components/ResumeEditor'
+  import ResumePreview from './components/ResumePreview'
+  import './assets/reset.css'
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        text: '你好'
+      }
+
+    },
+    components: {Topbar, ResumeEditor, ResumePreview}
+  }
+</script>
+
+<style>
+  p {
+
+  }
+</style>
