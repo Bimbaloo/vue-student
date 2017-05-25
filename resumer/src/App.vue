72f0ebdfd6cfa74dce5bf6332bde540fdbe241b8
mazao <1030102244@qq.com>
2017-05-25 09:12:01 +0800

    模块化完成

---
+++ resumer/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class='page'>
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
+</template>
+
+
+<script>
+  import 'normalize.css/normalize.css'
+  import './assets/reset.css'
+
+  import Topbar from './components/Topbar'
+  import ResumeEditor from './components/ResumeEditor'
+  import ResumePreview from './components/ResumePreview'
+
+
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        text: '你好'
+      }
+
+    },
+    components: {Topbar, ResumeEditor, ResumePreview}
+  }
+</script>
+
+<style>
+  .page {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background: #EAEBEC;
+  }
+
+  .page > main {
+    flex-grow: 1;
+  }
+
+  .page > main {
+    min-width: 1024px;
+    max-width: 1440px;
+    display: flex;
+    width: 100%; /* 试试不加这句会怎样 */
+    align-self: center;
+    justify-content: space-around;
+  }
+
+  #resumeEditor {
+    width: 35%;
+    background: #444;
+  }
+
+  #resumePreview {
+    width: 61.66667%;
+    background: #777;
+  }
+</style>
