0a62b3fc187b39cfef4ac78f1041b02816e99910
mazao <1030102244@qq.com>
2017-05-26 09:10:51 +0800

    项目开始

---
+++ resumer_mockups/README.md
@@ -0,0 +1,5 @@
+# 使用sass预编译
+      export SASS_BINARY_SITE="https://npm.taobao.org/mirrors/node-sass"
+      npm install --save  sass-loader node-sass
+
+
---
+++ resumer_mockups/src/components/ResumeEditor.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="resumeEditor">
+    <nav>
+      <ol>
+        <li v-for="(item,index) in resume.visibleItems" :class="{active: item === selected}"  @click="selected = item">
+          {{index}}
+        </li>
+      </ol>
+    </nav>
+    <ol class="panels">
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ResumeEditor',
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style lang="scss">
+  #resumeEditor{
+    background:#ffffff;
+    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+      display: flex;
+      flex-direction: row;
+      > nav{
+               width: 80px;
+               background: black;
+               color: white;
+        > ol {
+          > li{
+              height: 48px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              margin-top: 16px;
+              margin-bottom: 16px;
+            &.active{
+               background: white;
+               color: black;
+             }
+          }
+        }
+      }
+    }
+    ol{
+         list-style: none;
+     }
+</style>
