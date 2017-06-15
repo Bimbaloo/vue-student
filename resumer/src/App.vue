cdcb7fe9da78eca53611528e491348bd8df9026f
test <mazao@zjrealtech.com>
2017-06-15 14:31:35 +0800

    模块化复习

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -1,65 +1,65 @@
-<template>
-  <div class='page'>
-    <header>
-      <Topbar/>
-    </header>
-    <main>
-      <ResumeEditor/>
-      <ResumePreview/>
-    </main>
-  </div>
-</template>
-
+ <template>
+   <div class=page>
+     <header>
+       <Topbar/>
+     </header>
+     <main>
+       <ResumeEditor/>
+       <ResumePreview/>
+     </main>
+    </div>
+  </template>
 
 <script>
-  import 'normalize.css/normalize.css'
-  import './assets/reset.css'
+import 'normalize.css/normalize.css'
+import './assets/reset.css'
 
-  import Topbar from './components/Topbar'
-  import ResumeEditor from './components/ResumeEditor'
-  import ResumePreview from './components/ResumePreview'
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
 
 
-  export default {
-    name: 'app',
-    data: function () {
-      return {
-        text: '你好'
-      }
-
-    },
-    components: {Topbar, ResumeEditor, ResumePreview}
-  }
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      text: 'hello'
+    }
+  },
+  components: {Topbar, ResumeEditor, ResumePreview}
+}
 </script>
 
 <style>
-  .page {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    background: #EAEBEC;
-  }
-
-  .page > main {
-    flex-grow: 1;
-  }
-
-  .page > main {
-    min-width: 1024px;
-    max-width: 1440px;
-    display: flex;
-    width: 100%; /* 试试不加这句会怎样 */
-    align-self: center;
-    justify-content: space-around;
-  }
-
-  #resumeEditor {
-    width: 35%;
-    background: #444;
-  }
-
-  #resumePreview {
-    width: 61.66667%;
-    background: #777;
-  }
+  .page{
+     height: 100vh;
+     display: flex;
+     flex-direction: column;
+     background: #EAEBEC;
+   }
+   .page>main{
+     flex-grow: 1;  
+   }
+ 
+   .page>main{
+     min-width: 1024px;
+     max-width: 1440px;
+     margin-top: 16px;
+     margin-bottom: 16px;
+     display: flex;
+     justify-content: space-between;
+     padding: 0 16px;
+     width: 100%; /* 试试不加这句会怎样 */
+     align-self: center;
+   }
+ 
+   #resumeEditor{
+     width: 35%;
+     background: #444;
+   }
+   #resumePreview{
+     flex-grow: 1;
+     margin-left: 16px;
+     background: #777;
+   }
 </style>
